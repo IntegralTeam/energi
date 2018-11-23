87187e4d8ccb975034167ef86ed72e84024dd3e5
Egor Lysenko <elysenko@sfxdx.ru>
2018-11-23 14:02:39 +0700

    mn test: add test for calcRewardPoint()

--- consensus/masternode/masternode_reward_test.go
+++ consensus/masternode/masternode_reward_test.go
@@ -118,6 +118,75 @@ func getTestMasternodes_same_noReminder(num int) []*Masternode {
 	return masternodes
 }
 
+func Test_calcRewardPoint(t *testing.T) {
+	var round RewardsRound
+	// Test one-step round
+	round.Step = big.NewInt(1)
+	round.Length = big.NewInt(10)
+	for i := 0; i < 100; i++ {
+		point := calcRewardPoint(&round, big.NewInt(int64(i)))
+		assert.Equal(t, int(point.Uint64()), i % 10)
+	}
+
+	// Test 10-step round bounds
+	round.Step = big.NewInt(10)
+	round.Length = big.NewInt(20)
+	for i := 0; i < 1000; i++ {
+		point := calcRewardPoint(&round, big.NewInt(int64(i)))
+		assert.Equal(t, int(point.Uint64()) >= (i % 20) * 10, true)
+		assert.Equal(t, int(point.Uint64()) < (i % 20) * 10 + 10, true)
+	}
+
+	// Test the equiprobability of the distribution
+	round.Step = big.NewInt(10)
+	round.Length = big.NewInt(3)
+
+	pointsHits := make(map[uint64]int) // point -> number of occurrences
+	for i := 0; i < 10000; i++ {
+		point := calcRewardPoint(&round, big.NewInt(int64(i)))
+		_, ok := pointsHits[point.Uint64()]
+		if !ok {
+			pointsHits[point.Uint64()] = 0
+		}
+		pointsHits[point.Uint64()] += 1
+	}
+	for _, hits := range pointsHits {
+		assert.Equal(t, hits > 300 - 60, true)
+		assert.Equal(t, hits < 300 + 60, true)
+	}
+
+	// Test specific 1e+32-step round
+	round.Step = new(big.Int).Mul(big.NewInt(1e+16), big.NewInt(1e+16))
+	round.Length = big.NewInt(20)
+	point0 := calcRewardPoint(&round, big.NewInt(0))
+	point5 := calcRewardPoint(&round, big.NewInt(5))
+	point19 := calcRewardPoint(&round, big.NewInt(19))
+	point20 := calcRewardPoint(&round, big.NewInt(20))
+	point25 := calcRewardPoint(&round, big.NewInt(25))
+	point39 := calcRewardPoint(&round, big.NewInt(39))
+
+	//println(point0.String())
+	//println(point5.String())
+	//println(point19.String())
+	//println(point20.String())
+	//println(point25.String())
+	//println(point39.String())
+
+	point0_want,  _ := new(big.Int).SetString(  "48198034993379397001115665086549", 10)
+	point5_want,  _ := new(big.Int).SetString( "548198034993379397001115665086549", 10)
+	point19_want, _ := new(big.Int).SetString("1948198034993379397001115665086549", 10)
+	point20_want, _ := new(big.Int).SetString(  "92190392920402856263689962707065", 10)
+	point25_want, _ := new(big.Int).SetString( "592190392920402856263689962707065", 10)
+	point39_want, _ := new(big.Int).SetString("1992190392920402856263689962707065", 10)
+
+	assert.Equal(t, point0.Cmp(point0_want), 0)
+	assert.Equal(t, point5.Cmp(point5_want), 0)
+	assert.Equal(t, point19.Cmp(point19_want), 0)
+	assert.Equal(t, point20.Cmp(point20_want), 0)
+	assert.Equal(t, point25.Cmp(point25_want), 0)
+	assert.Equal(t, point39.Cmp(point39_want), 0)
+}
+
 func Test_filterNotActiveMasternodes_3_normal(t *testing.T) {
 	masternodes := getTestMasternodes_3_normal()
 	masternodes[2].ActivationBlockNumber.Mul(big.NewInt(1e+16), big.NewInt(1e+16))
